7c2a39f42634a48f4ee90880707daef38b84c933
green1490 <zambokrisztian1490@gmail.com>
2022-04-18 15:16:52 +0200

    destroy current activity with the arrow option

--- app/src/main/java/org/tensorflow/codelabs/objectdetection/SettingsActivity.kt
+++ app/src/main/java/org/tensorflow/codelabs/objectdetection/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package org.tensorflow.codelabs.objectdetection
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
@@ -9,13 +10,27 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId) {
+            R.id.home -> {this.finish(); true
+            }
+            else -> {this.finish();true
+            }
+        }
+
+        //return super.onOptionsItemSelected(item)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
